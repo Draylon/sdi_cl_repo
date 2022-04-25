02d2a6990d281582eeba14af8f8645306af1c059
Draylon Vieira <draylid@gmail.com>
2022-04-25 00:44:45 -0300

    show3

--- c2/hw_server.c
+++ c2/hw_server.c
@@ -91,8 +91,8 @@ int *solicitapeca_1_svc(struct peca_req *pecareq,struct svc_req *req){
      int qt = pecareq->qt;
      for(int q1 = 0;q1 < qt;q1++){
           for(int i=0;i<npecas;i++){
-               printf("req:%c %i\n",id,qt);
                if(pecas[i] == id){
+                    printf("req:%c %i\n",id,qt);
                     pecas[i] = (char)32;
                     
                     entregas[nentr] = id;
