7a790afb3690fafa32e223413e19457d8ccb0d95
Draylon Vieira <draylid@gmail.com>
2022-05-08 22:50:43 -0300

    update_rabbitmq

--- coelhoMQ/06-RabbitMQJava/Send.java
+++ coelhoMQ/06-RabbitMQJava/Send.java
@@ -1,6 +1,9 @@
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+
+import java.util.Date;
 
 public class Send {
 
@@ -13,8 +16,14 @@ public class Send {
     Channel channel = connection.createChannel();
 
     channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+    AMQP.BasicProperties p = new AMQP.BasicProperties(null,null,null,null,2,null,null,null,null, new Date(),null,null,null,null);
+
     String message = "Hello World!";
     channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+    System.out.println(" [x] Sent '" + message + "'");
+    message = "Special Hello World!";
+    channel.basicPublish("", QUEUE_NAME, p, message.getBytes("UTF-8"));
     System.out.println(" [x] Sent '" + message + "'");
 
     channel.close();
